9fdeaf67c460765d935fd0770f7c34a0e24b2afe
babyboydaprince <bala_zxz@hotmail.com>
2024-05-14 21:17:51 -0300

    (add) GO INTENSIVO

--- main.go
+++ main.go
@@ -6,42 +6,42 @@ import (
 	"net/http"
 )
 
+type Car struct {
+	Name  string `json:"name"`
+	Model string `json:"model"`
+	Year  int    `json:"year"`
+}
+
+func (c Car) Move() {
+	fmt.Println("The car, " + c.Name + " is moving...")
+}
+
+func (c Car) Stop() {
+	fmt.Println("The car, " + c.Name + " stopped...")
+}
+
 func main() {
-	// Variable creation :=  vs attribution =
-	type a int // type a = int
+	car1 := Car{Name: "Ford", Model: "Mustang", Year: 1969}
+	car2 := Car{Name: "Chevrolet", Model: "Camaro", Year: 1969}
 
-	type Car struct {
-		Name  string
-		Model string
-		Year  int
-	}
+	// Simple http server
+	//print("Hello, World\n")
+	//print("Starting web server...")
+	//
+	//err := http.ListenAndServe(":8080", nil)
+	//if err != nil {
+	//	fmt.Println("Error starting server:", err)
+	//	return
+	//}
 
-	func(c Car) Move()
-	{
-		fmt.Println("The car, " + c.Name + " is moving...")
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		err := json.NewEncoder(w).Encode(car1)
+		if err != nil {
+			fmt.Println("Error encoding car:", err) // Added error handling
+			return
+		}
+	})
 
-	func(c Car) Stop()
-	{
-		fmt.Println("The car, " + c.Name + " stopped...")
-	}
-
-	main()
-	{
-		car1 := Car{Name: "Ford", Model: "Mustang", Year: 1969}
-		car2 := Car{Name: "Chevrolet", Model: "Camaro: 1969"}
-
-		// Simple http server
-		print("Hello, World\n")
-		print("Starting web server...")
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			err := json.NewEncoder(w).Encode(car1)
-			if err != nil {
-				return
-			}
-	}
-		http.ListenAndServe(":8080", nil)
-	}
-
-
+	car1.Move()
+	car2.Stop()
 }
